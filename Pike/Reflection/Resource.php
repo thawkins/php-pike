ec68b9fd49d3f9922aa7cc92efedba20999f6aad
kees <kees@stichtingsbo.nl>
2011-08-24 21:39:14 +0000

    Small fix with module support

--- Pike/Reflection/Resource.php
+++ Pike/Reflection/Resource.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (C) 2011 by Pieter Vogelaar (platinadesigns.nl) and Kees Schepers (keesschepers.nl)
  *
@@ -32,15 +33,14 @@
  * @copyright  Copyright (C) 2011 by Pieter Vogelaar (platinadesigns.nl) and Kees Schepers (keesschepers.nl)
  * @license    MIT
  */
-class Pike_Reflection_Resource
-{
+class Pike_Reflection_Resource {
+
     /**
      * Attributes
      *
      * @var array
      */
     protected $_attributes = array();
-
     /**
      * FrontController
      *
@@ -53,11 +53,9 @@ class Pike_Reflection_Resource
      *
      * @param array $attributes
      */
-    public function __construct(array $attributes = array())
-    {
+    public function __construct(array $attributes = array()) {
         $defaultAttributes = array('roles', 'human');
-        $this->_attributes = array_keys(array_merge(array_flip($defaultAttributes),
-            array_flip($attributes)));
+        $this->_attributes = array_keys(array_merge(array_flip($defaultAttributes), array_flip($attributes)));
     }
 
     /**
@@ -69,8 +67,7 @@ class Pike_Reflection_Resource
      * @param  string $actionName
      * @return string
      */
-    protected function _getInflectedActionName($actionName)
-    {
+    protected function _getInflectedActionName($actionName) {
         $inflectedActionName = '';
         for ($i = 0; $i < strlen($actionName); $i++) {
             if ($actionName[$i] == strtoupper($actionName[$i])) {
@@ -87,15 +84,17 @@ class Pike_Reflection_Resource
      *
      * @return array
      */
-    public function toArray()
-    {
+    public function toArray() {
         $resources = array();
         $this->_frontController = Zend_Controller_Front::getInstance();
-
-        // Retrieve the controller directory
+        
+        // Retrieve the controller directory        
         $controllerDirectories = $this->_frontController->getControllerDirectory();
         foreach ($controllerDirectories as $moduleName => $controllerDirectory) {
 
+            if ((bool) $this->_frontController->getParam('prefixDefaultModule') || $moduleName !== 'default') {
+                $classPrefix = ucfirst($moduleName) . '_';
+            }
             // Iterate over controllers found in the controller directory
             $directoryIterator = new DirectoryIterator($controllerDirectory);
             foreach ($directoryIterator as $file) {
@@ -106,10 +105,10 @@ class Pike_Reflection_Resource
 
                 // Initialize the reflection of the controller class
                 require_once $file->getPathname();
-                $className = $file->getBasename('.php');
-                $controllerName = $file->getBasename('Controller.php');
-                $classReflection = new Zend_Reflection_Class($className);
-
+                $className = $classPrefix . $file->getBasename('.php');
+                $controllerName = $file->getBasename('Controller.php');                
+                $classReflection = new Zend_Reflection_Class($className);                
+                
                 // Iterate over the public methods of the controller class
                 $methods = $classReflection->getMethods(ReflectionMethod::IS_PUBLIC);
                 foreach ($methods as $method) {
@@ -120,7 +119,7 @@ class Pike_Reflection_Resource
                         $resourceAttributes = $this->_getResourceAttributesFromActionMethod($method);
                         $actionName = $this->_getInflectedActionName(substr($method->name, 0, -6));
                         $resources[$moduleName][strtolower($controllerName)][$actionName]
-                            = $resourceAttributes;
+                                = $resourceAttributes;
                     }
                 }
             }
@@ -138,8 +137,7 @@ class Pike_Reflection_Resource
      * @param  string $moduleName
      * @return array
      */
-    public function toFlatArray($moduleName = 'default')
-    {
+    public function toFlatArray($moduleName = 'default') {
         $flatResources = array();
         $resources = $this->toArray();
 
@@ -162,15 +160,14 @@ class Pike_Reflection_Resource
      * @param  array $resources
      * @return array
      */
-    protected function _excludeErrorHandlerResource($resources)
-    {
+    protected function _excludeErrorHandlerResource($resources) {
         /* @var $errorHandler Zend_Controller_Plugin_ErrorHandler */
         $errorHandler = Zend_Controller_Front::getInstance()->getPlugin('Zend_Controller_Plugin_ErrorHandler');
 
         // Access is always granted to the error handler, so it can be ignored as resource
-        $module =  $errorHandler->getErrorHandlerModule();
-        $controller =  $errorHandler->getErrorHandlerController();
-        $action =  $errorHandler->getErrorHandlerAction();
+        $module = $errorHandler->getErrorHandlerModule();
+        $controller = $errorHandler->getErrorHandlerController();
+        $action = $errorHandler->getErrorHandlerAction();
         unset($resources[$module][$controller][$action]);
 
         return $resources;
@@ -182,8 +179,7 @@ class Pike_Reflection_Resource
      * @param  Zend_Reflection_Method $actionMethod
      * @return array
      */
-    protected function _getResourceAttributesFromActionMethod(Zend_Reflection_Method $actionMethod)
-    {
+    protected function _getResourceAttributesFromActionMethod(Zend_Reflection_Method $actionMethod) {
         $resourceAttributes = array();
         $docComment = $actionMethod->getDocComment();
 
@@ -223,4 +219,5 @@ class Pike_Reflection_Resource
 
         return $resourceAttributes;
     }
+
 }
